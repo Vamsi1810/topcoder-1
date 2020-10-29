357d40a2174343a184e529cec7cd30e6e5dfbf0c
Vamsi1810 <66599228+Vamsi1810@users.noreply.github.com>
2020-10-30 00:06:14 +0530

    2nd task completed

--- topcoder-learning-angular/src/app/mock-treasures.ts
+++ topcoder-learning-angular/src/app/mock-treasures.ts
@@ -1,5 +1,10 @@
-export interface Treasure{
+interface Treasure{
     id:Number,
     name:String,
     location:String
 }
+export const treasure: Treasure = {
+    id: 1,
+    name: 'Windstorm Gold Staff',
+    location: 'Tree behind home',
+  };
--- topcoder-learning-angular/src/app/treasures/treasures.component.ts
+++ topcoder-learning-angular/src/app/treasures/treasures.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Treasure } from '../mock-treasures'
+import { treasure } from '../mock-treasures'
 @Component({
   selector: 'app-treasures',
   templateUrl: './treasures.component.html',
   styleUrls: ['./treasures.component.css']
 })
 export class TreasuresComponent implements OnInit {
-  treasure: Treasure = {
-    id: 1,
-    name: 'Windstorm Gold Staff',
-    location: 'Tree behind home',
-  };
+  treasure=treasure;
   constructor() { }
    
   ngOnInit(): void {
